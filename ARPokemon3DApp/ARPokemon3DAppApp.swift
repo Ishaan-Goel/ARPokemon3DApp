558b53e855676fc9f5c4f859b97eb77575b412d4
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 17:49:18 -0800

    Initial Commit

---
+++ ARPokemon3DApp/ARPokemon3DAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARPokemon3DAppApp.swift
+//  ARPokemon3DApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ARPokemon3DAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
